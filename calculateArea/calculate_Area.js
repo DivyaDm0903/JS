55e5ccd22ba589342768bf1491dc36b1140846a5
DivyaDm0903 <divya.dm@infogain.com>
2026-04-16 07:12:40 -0400

    practice

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -1,5 +1,8 @@
 let length;
 let width;
+let grocery1;
+let grocery2;
+let grocery3;
 function calculateArea() {
  length = parseFloat(document.getElementById('length').value);
  width = parseFloat(document.getElementById('width').value);
@@ -7,5 +10,16 @@ function calculateArea() {
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 
 }
+function TotalGrocery(){
+    grocery1 = parseFloat(document.getElementById('grocery-1').value);
+    grocery2 = parseFloat(document.getElementById('grocery-2').value);
+    grocery3 = parseFloat(document.getElementById('grocery-3').value);
+    
+ let total = grocery1 + grocery2 + grocery3;
+ document.getElementById('totalGrocery').innerText = `The total amount  is: ${total}`;
+
+}
+
+
 
  
