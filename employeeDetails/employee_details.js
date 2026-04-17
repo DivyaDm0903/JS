36f987348ca04199c47a5df2a47b453da48d7b2d
DivyaDm0903 <divya.dm@infogain.com>
2026-04-17 01:37:42 -0400

    arraymethods

---
+++ employeeDetails/employee_details.js
@@ -0,0 +1,47 @@
+const employees = [
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 ,specification : 'Javascript'},
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 ,specification : 'Python'},
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 , specification : 'Java'},
+      //... More employee records can be added here
+    ];
+
+// Function to display all employees
+function displayEmployees() {
+    const totalEmployees = employees
+        .map(employee => `<p>${employee.id}: ${employee.name} - ${employee.department} - ${employee.salary} - ${employee.specification}</p>`)
+        .join('');
+    document.getElementById('employeesDetails').innerHTML = totalEmployees;
+}
+
+function calculateTotalSalaries() {
+    const totalSalaries = employees.reduce((acc, employee) => acc + employee.salary, 0);
+    alert(`Total Salaries: ${totalSalaries}`);
+  }
+
+  function displayHREmployees() {
+    const hrEmployees = employees.filter(employee => employee.department === 'HR');
+     const hrEmployeesDisplay = hrEmployees.map((employee, index) => `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - ${employee.salary} - ${employee.specification}</p>`).join('');
+     document.getElementById('employeesDetails').innerHTML = hrEmployeesDisplay;
+}
+function findEmployeeById(employeeId) {
+    const foundEmployee = employees.find(employee => employee.id === employeeId);
+    if (foundEmployee) {
+    document.getElementById('employeesDetails').innerHTML =`<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - ${foundEmployee.salary}</p>`;
+    }
+    else{
+      document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+     }
+ }
+ function findEmployeeBySpecification(specification){
+    const foundEmployeeSpecification=employees.find(employee => employee.specification === specification );
+    console.log("hi",foundEmployeeSpecification);
+    if(foundEmployeeSpecification){
+        document.getElementById('employeesDetails').innerHTML = `<p> ${foundEmployeeSpecification.id} : ${foundEmployeeSpecification.name} - ${foundEmployeeSpecification.department} - ${foundEmployeeSpecification.specification}`
+
+    }
+    else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
+
+    }
+
+ }
